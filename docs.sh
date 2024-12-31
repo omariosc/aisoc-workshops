f58df0b4a51145a73a0364c4256bf9eca06feea6
Omar Choudhry <sc20osc@leeds.ac.uk>
2024-12-31 00:51:28 +0000

    Removed commented text

--- docs.sh
+++ docs.sh
@@ -1,10 +1,3 @@
-# 1. [Workshop 1: Introduction to AI](/1%20Introduction%20to%20AI/README.md)
-# 2. [Workshop 2: Building a Machine Learning Pipeline](/2%20Buliding%20a%20Machine%20Learning%20Pipeline/README.md)
-# 3. [Workshop 3: Neural Networks](/3%20Neural%20Networks/README.md)
-# 4. [Workshop 4: Ethics of AI](/4%20Ethics%20of%20AI/README.md)
-# 5. [Workshop 5: AI Mini-Hackathon](/5%20AI%20Mini-Hackathon/README.md)
-# 6. [Workshop 6: AI Project Showcase](/6%20AI%20Project%20Showcase/README.md)
-
 # copy each as workshopX.md
 cp 1\ Introduction\ to\ AI/README.md docs/workshop1.md
 cp 2\ Buliding\ a\ Machine\ Learning\ Pipeline/README.md docs/workshop2.md
