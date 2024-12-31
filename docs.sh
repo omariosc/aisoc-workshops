0fa8cc130534a001d394a7c28f7e86cf1f868eda
Omar Choudhry <sc20osc@leeds.ac.uk>
2024-12-31 00:55:44 +0000

    Changed to paths with ?s

--- docs.sh
+++ docs.sh
@@ -1,9 +1,10 @@
-cp "1 Introduction to AI/README.md" docs/workshop1.md
-cp "2 Buliding a Machine Learning Pipeline/README.md" docs/workshop2.md
-cp "3 Neural Networks/README.md" docs/workshop3.md
-cp "4 Ethics of AI/README.md" docs/workshop4.md
-cp "5 AI Mini-Hackathon/README.md" docs/workshop5.md
-cp "6 AI Project Showcase/README.md" docs/workshop6.md
+cp 1?Introduction?to?AI/README.md docs/workshop1.md
+cp 2?Buliding?a?Machine?Learning?Pipeline/README.md docs/workshop2.md
+cp 3?Neural?Networks/README.md docs/workshop3.md
+cp 4?Ethics?of?AI/README.md docs/workshop4.md
+cp 5?AI?Mini-Hackathon/README.md docs/workshop5.md
+cp 6?AI?Project?Showcase/README.md docs/workshop6.md
+
 cp index.md docs/index.md
 cp LICENSE docs/LICENSE.md
 
