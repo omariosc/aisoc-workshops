552d75b4cf545e0d50f17f5e5a6772c70162d33a
Omar Choudhry <sc20osc@leeds.ac.uk>
2024-12-31 00:52:39 +0000

    Changed to stringed paths

--- docs.sh
+++ docs.sh
@@ -1,10 +1,9 @@
-# copy each as workshopX.md
-cp 1\ Introduction\ to\ AI/README.md docs/workshop1.md
-cp 2\ Buliding\ a\ Machine\ Learning\ Pipeline/README.md docs/workshop2.md
-cp 3\ Neural\ Networks/README.md docs/workshop3.md
-cp 4\ Ethics\ of\ AI/README.md docs/workshop4.md
-cp 5\ AI\ Mini-Hackathon/README.md docs/workshop5.md
-cp 6\ AI\ Project\ Showcase/README.md docs/workshop6.md
+cp "1 Introduction to AI/README.md" docs/workshop1.md
+cp "2 Buliding a Machine Learning Pipeline/README.md" docs/workshop2.md
+cp "3 Neural Networks/README.md" docs/workshop3.md
+cp "4 Ethics of AI/README.md" docs/workshop4.md
+cp "5 AI Mini-Hackathon/README.md" docs/workshop5.md
+cp "6 AI Project Showcase/README.md" docs/workshop6.md
 cp index.md docs/index.md
 cp LICENSE docs/LICENSE.md
 
