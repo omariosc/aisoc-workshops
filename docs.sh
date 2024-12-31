b4bb397c42ea8d5f3df1324e293d7421c807a851
Omar Choudhry <sc20osc@leeds.ac.uk>
2024-12-31 01:12:38 +0000

    Built site using mkdocs statically

--- docs.sh
+++ docs.sh
@@ -1,15 +1,16 @@
-cp 1?Introduction?to?AI/README.md docs/workshop1.md
-cp 2?Buliding?a?Machine?Learning?Pipeline/README.md docs/workshop2.md
-cp 3?Neural?Networks/README.md docs/workshop3.md
-cp 4?Ethics?of?AI/README.md docs/workshop4.md
-cp 5?AI?Mini-Hackathon/README.md docs/workshop5.md
-cp 6?AI?Project?Showcase/README.md docs/workshop6.md
+mkdir -p docs
 
-cp index.md docs/index.md
+cp 1\ Introduction\ to\ AI/README.md docs/workshop1.md
+cp 2\ Buliding\ a\ Machine\ Learning\ Pipeline/README.md docs/workshop2.md
+cp 3\ Neural\ Networks/README.md docs/workshop3.md
+cp 4\ Ethics\ of\ AI/README.md docs/workshop4.md
+cp 5\ AI\ Mini-Hackathon/README.md docs/workshop5.md
+cp 6\ AI\ Project\ Showcase/README.md docs/workshop6.md
+
 cp LICENSE docs/LICENSE.md
+
+# Run MkDocs build
+python -m mkdocs build
 
 # Run MkDocs serve
 # python -m mkdocs serve &
-# Run MkDocs build
-pip install mkdocs
-python -m mkdocs build
